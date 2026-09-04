c1ce70829c488c2c6ccb033909c5d50f50ac3cc8
Manjitha-MK <manjithataridu123@gmail.com>
2026-09-04 12:32:31 +0530

    problem 4 and 5 is done

--- day1.py
+++ day1.py
@@ -8,17 +8,50 @@
 # print("Height:", height)
 # print("Is student:", is_student)
 
+# -----------------------------------------------------------------------------
+
+# N1 = int(input("Enter first number: "))
+# N2 = int(input("Enter second number: "))
+
+# addition = N1 + N2
+# print("Addition:", addition)
+
+# subtraction = N1 - N2
+# print("Subtraction:", subtraction)
+
+# multiplication = N1 * N2
+# print("Multiplication:", multiplication)
+
+# division = N1 / N2
+# print("Division:", division)
+
+# ------------------------------------------------------------------------------
+
+# age = int(input("Enter your age: "))
+
+# if age >=18:
+#     print("You can vote")
+# else:
+#     print("You cannot vote")
+
+# -------------------------------------------------------------------------------
+
+# X = int(input("Enter a number: "))
+
+# if X % 2 == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+# --------------------------------------------------------------------------------
+
 N1 = int(input("Enter first number: "))
 N2 = int(input("Enter second number: "))
+N3 = int(input("Enter third number: "))
 
-addition = N1 + N2
-print("Addition:", addition)
-
-subtraction = N1 - N2
-print("Subtraction:", subtraction)
-
-multiplication = N1 * N2
-print("Multiplication:", multiplication)
-
-division = N1 / N2
-print("Division:", division)
+if N1 >= N2 and N1 >= N3:
+    print("The largest number is:", N1)
+elif N2 >= N1 and N2 >= N3:
+    print("The largest number is:", N2)
+else:
+    print("The largest number is:", N3)
