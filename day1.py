c58f4c08abe0c7419afb1982ebe4124436fca9b1
Manjitha-MK <manjithataridu123@gmail.com>
2026-09-04 13:11:01 +0530

    problem 7 completed

--- day1.py
+++ day1.py
@@ -58,19 +58,39 @@
 
 # --------------------------------------------------------------------------------
 
-marks = int(input("Enter your marks: "))
+# marks = int(input("Enter your marks: "))
 
-if marks < 0 or marks > 100:
-    print("Invalid marks")
-elif marks >= 75 and marks <= 100:
-    print("Grade: A")
-elif marks >= 65:
-    print("Grade: B")
-elif marks >= 55:
-    print("Grade: C")
-elif marks >= 45:
-    print("Grade: S")
-else:
-    print("Grade: F")
+# if marks < 0 or marks > 100:
+#     print("Invalid marks")
+# elif marks >= 75 and marks <= 100:
+#     print("Grade: A")
+# elif marks >= 65:
+#     print("Grade: B")
+# elif marks >= 55:
+#     print("Grade: C")
+# elif marks >= 45:
+#     print("Grade: S")
+# else:
+#     print("Grade: F")
 
 # ----------------------------------------------------------------------------------
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+n3 = int(input("Enter third number: "))
+
+avg = (n1 + n2 + n3)/3
+print("The average of the three numbers is:", avg)
+
+if n1 >= n2 and n1 >= n3:
+    largest = n1
+    smallest = n2 if n2 <= n3 else n3
+elif n2 >= n1 and n2 >= n3:
+    largest = n2
+    smallest = n1 if n1 <= n3 else n3
+else:
+    largest = n3
+    smallest = n1 if n1 <= n2 else n2
+
+print("The largest number is:", largest)
+print("The smallest number is:", smallest)
