ca6b52ea7763a09b3b4bab269bfda8886c4ab05f
Manjitha-MK <manjithataridu123@gmail.com>
2026-09-04 12:16:54 +0530

    probblem 1 and 2 done

--- day1.py
+++ day1.py
@@ -1,9 +1,24 @@
-name = "Manjitha"
-age = 25
-height = "180cm"
-is_student = True
+# name = "Manjitha"
+# age = 25
+# height = "180cm"
+# is_student = True
 
-print("Name:", name)
-print("Age:", age)
-print("Height:", height)
-print("Is student:", is_student)
+# print("Name:", name)
+# print("Age:", age)
+# print("Height:", height)
+# print("Is student:", is_student)
+
+N1 = int(input("Enter first number: "))
+N2 = int(input("Enter second number: "))
+
+addition = N1 + N2
+print("Addition:", addition)
+
+subtraction = N1 - N2
+print("Subtraction:", subtraction)
+
+multiplication = N1 * N2
+print("Multiplication:", multiplication)
+
+division = N1 / N2
+print("Division:", division)
