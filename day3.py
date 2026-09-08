53049efbf23de857b8ff2cafe7fb833bf7bcc7ce
Manjitha-MK <manjithataridu123@gmail.com>
2026-09-08 10:32:00 +0530

    set {}

--- day3.py
+++ day3.py
@@ -34,3 +34,14 @@ print(fruits[1])
 print(fruits[-1])
 
 # Tuple is useful when the data should stay fixed. Tuple can't be change -> fruits[0] = grapes XXX
+
+# 3. Set {}
+
+fruits = {"apple", "banana", "apple", "orange", "banana"}
+
+print(fruits)
+fruits.add("mango")
+fruits.remove("banana")
+print(fruits)
+
+# The duplicates are removed. Doesn't work: fruits[0]   # ❌ because set is unordered and doesn't support indexing.
