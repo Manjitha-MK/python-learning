20ce5ddb54e155ecbad8d6a720896df954390960
Manjitha-MK <manjithataridu123@gmail.com>
2026-09-11 12:05:56 +0530

    find largest numbers

--- day4.py
+++ day4.py
@@ -1,45 +1,68 @@
-def greet():
-    print("hello")
+# def greet():
+#     print("hello")
 
-greet()
+# greet()
 
-def hello(name):
-    print("Hello", name)
+# def hello(name):
+#     print("Hello", name)
 
-hello("Manjitha")
-hello("Kasun")
+# hello("Manjitha")
+# hello("Kasun")
 
-def add(a, b):
-    print(a + b)
+# def add(a, b):
+#     print(a + b)
 
-add(10, 20)
-add(39, 2)
+# add(10, 20)
+# add(39, 2)
 
-def multiply(x, y):
-    return x * y
+# def multiply(x, y):
+#     return x * y
 
-results = multiply(10, 2)
-new_results = results * 2
+# results = multiply(10, 2)
+# new_results = results * 2
 
-print(new_results)
+# print(new_results)
 
-def square(number):
-    return number * number
+# def square(number):
+#     return number * number
 
-x = square(5)
-y= square(3)
+# x = square(5)
+# y= square(3)
 
-print("add square value:",x + y)
+# print("add square value:",x + y)
 
-def is_even(number):
-    # if number % 2 == 0:
-    #     return True
-    # else:
-    #     return False
-    return number % 2 == 0
+# def is_even(number):
+#     # if number % 2 == 0:
+#     #     return True
+#     # else:
+#     #     return False
+#     return number % 2 == 0
 
-print(is_even(22))
-print(is_even(13))
+# print(is_even(22))
+# print(is_even(13))
+
+# def find_max(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+
+# print(find_max(10, 20))
+# print(find_max(29, 30))
+
+numbers = [10, 25, 7, 42, 18]
+
+
+def find_largest(numbers):
+    largest = 0
+
+    for number in numbers:
+        if number > largest:
+            largest = number
+
+    return largest
+        
+print(find_largest(numbers))
 
         
 
