c4b7e255dd0f4a90edb6e7898cce0bcebb24069c
Manjitha-MK <manjithataridu123@gmail.com>
2026-09-11 11:34:44 +0530

    Write a function called is_even(number) that returns True if the number is even and False if it is odd.

--- day4.py
+++ day4.py
@@ -30,3 +30,15 @@ x = square(5)
 y= square(3)
 
 print("add square value:",x + y)
+
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+print(is_even(22))
+print(is_even(13))
+
+        
+
